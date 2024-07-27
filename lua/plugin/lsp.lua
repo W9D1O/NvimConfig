221f55eeff5bc3566554ebdfbf524bbddbb92e0b
damian <damian.otazu91@gmail.com>
2024-07-27 14:49:46 -0300

    Me dejo de joder con nvim

--- lua/plugin/lsp.lua
+++ lua/plugin/lsp.lua
@@ -1,4 +1,3 @@
-
 return {
   "neovim/nvim-lspconfig",
       opts = function()
@@ -9,7 +8,7 @@ return {
     "folke/neodev.nvim",
   },
   config = function()
-    vim.keymap.set('n', '<space>e', vim.diagnostic.open_float)
+    vim.keymap.set('n', '<space>de', vim.diagnostic.open_float)
     vim.keymap.set('n', '[d', vim.diagnostic.goto_prev)
     vim.keymap.set('n', ']d', vim.diagnostic.goto_next)
     vim.keymap.set('n', '<space>q', vim.diagnostic.setloclist)
